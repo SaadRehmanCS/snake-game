4fd437970b9357998be0b5f6fd97e3d787183f2c
Saad Rehman <saad@dhcp-128-189-85-4.ubcsecure.wireless.ubc.ca>
2023-02-22 18:43:20 -0800

    basic app works

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -3,6 +3,9 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
 import Snake
 
+offset :: Int
+offset = 100
+
 window :: Display
 window = InWindow "Snake Game" (width, height) (offset, offset)
 
@@ -10,33 +13,41 @@ background :: Color
 background = black
 
 render :: SnakeGame -> Picture
-render game = pictures $ [ fillRectangle black (16, 0) (640, 20)
-                                , fillRectangle black (16, 24) (640, 20)
-                                , fillRectangle black (0, 12) (20, 480)
-                                , fillRectangle black (32, 12) (20, 480) ] ++
-                                  fmap (convertToPicture black) snake ++ 
+render game = pictures $ [ fillRectangle blue (gameBorderWidthHalf, 0) (borderWidth, blockSize)
+                                , fillRectangle blue (gameBorderWidthHalf, fromIntegral gameBorderHeight) (borderWidth, blockSize)
+                                , fillRectangle blue (0, gameBorderHeightHalf) (blockSize, borderHeight)
+                                , fillRectangle blue (fromIntegral gameBorderWidth, gameBorderHeightHalf) (blockSize, borderHeight) ] ++
+                                  fmap (convertToPicture green) snake ++ 
                                   fmap (convertToPicture blue) [food] ++
                                   gameOverPicture
                 where
-                    snake = getSnake game 
-                    food = getFood game
-                    convertToPicture :: Color -> (Int, Int) -> Picture
-                    convertToPicture color' (x, y) = fillRectangle color' (toFloat (x, y)) (20, 20)
-                    fillRectangle color' (tx, ty) (w, h) =  color color' $ 
-                                                    scale 1 (-1) $ 
-                                                    translate (tx * 20 - 320) (ty * 20 - 240) $ 
-                                                    rectangleSolid w h
-                    toFloat (x, y) = (fromIntegral x, fromIntegral y)
-                    gameOverPicture =   if (gameOver game) 
-                                then [  color blue $ 
-                                        translate (-200) (0) $ 
-                                        scale 0.5 0.5 $ 
-                                        text "GAME OVER"
-                                     ,  color blue $ 
-                                        translate (-175) (-50) $ 
-                                        scale 0.2 0.2 $ 
-                                        text "Press SPACE to try again." ] 
-                                else []
+                        gameBorderHeightHalf = fromIntegral (gameBorderHeight `div` 2)
+                        gameBorderWidthHalf = fromIntegral (gameBorderWidth `div` 2)
+                        borderWidth = fromIntegral width-blockSize
+                        borderHeight = fromIntegral height-blockSize
+                        snake = getSnake game 
+                        food = getFood game
+                        convertToPicture :: Color -> (Int, Int) -> Picture
+                        convertToPicture color' (x, y) = fillRectangle color' (toFloat (x, y)) (20, 20)
+                        fillRectangle color' (tx, ty) (w, h) =  color color' $ 
+                                                        scale 1 (-1) $ 
+                                                        translate (tx * blockSize - (borderWidth / 2)) (ty * blockSize - (borderHeight / 2)) $ 
+                                                        rectangleSolid w h
+                        toFloat (x, y) = (fromIntegral x, fromIntegral y)
+                        gameOverPicture =   if (gameOver game) 
+                                        then [  color blue $ 
+                                                translate (-200) (0) $ 
+                                                scale 0.5 0.5 $ 
+                                                text "GAME OVER"
+                                        ,  color blue $ 
+                                                translate (-175) (-50) $ 
+                                                scale 0.2 0.2 $ 
+                                                text "Press SPACE to play again."
+                                        , color red $ 
+                                                translate (-90) (-100) $ 
+                                                scale 0.3 0.3 $ 
+                                                text ("Score: "  ++ show (score game))]
+                                        else []
 
 handleKeys :: Event -> SnakeGame -> SnakeGame
 handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) snakeGame = (changeDirection snakeGame LEFT)
@@ -50,7 +61,21 @@ handleKeys (EventKey (SpecialKey KeySpace) Down _ _) snakeGame = if (gameOver sn
 handleKeys _ snakeGame = snakeGame
 
 update :: Float -> SnakeGame -> SnakeGame
-update time snakeGame = snakeGame
+update time snakeGame = if (gameOver snakeGame)
+        then (snakeGame)
+        else (Game newSnake newFood newGameOver curDirection newSeed newScore)
+        where
+                newGameOver = checkGameOver snakeGame
+                curSnake = getSnake snakeGame
+                curFood = getFood snakeGame
+                newSnake = move snakeGame
+                seed = randSeed snakeGame
+                (isFoodEaten, newScore) = foodIsEaten snakeGame
+                (genFood, newSeed) = generateFood snakeGame
+                newFood = if isFoodEaten
+                        then genFood
+                        else curFood
+                curDirection = direction snakeGame
 
 main :: IO ()
-main = play window background 10 (initialState True) render handleKeys update
+main = play window background 20 (initialState True) render handleKeys update
--- src/app/Snake.hs
+++ src/app/Snake.hs
@@ -1,13 +1,18 @@
 module Snake where
 import Data.Map as Map
+import System.Random
 
-data CurDir = UP | DOWN | LEFT | RIGHT deriving Show
+data CurDir = UP | DOWN | LEFT | RIGHT deriving (Show, Eq)
 
-width, height, offset :: Int
-width = 600
-height = 600
-offset = 100
-snakeLength = 20
+gameBorderHeight = 24
+gameBorderWidth = 32
+
+blockSize :: Float
+blockSize = 20
+
+width, height :: Int
+width = gameBorderWidth * 20 + (round blockSize)
+height = gameBorderHeight * 20 + (round blockSize)
 
 type Food = (Int, Int)
 type Snake = [Food]
@@ -16,19 +21,59 @@ data SnakeGame = Game {
     getSnake :: Snake,
     getFood :: Food,
     gameOver :: Bool,
-    direction :: CurDir
+    direction :: CurDir,
+    randSeed :: StdGen,
+    score :: Int
 } deriving Show
 
 initialState :: Bool -> SnakeGame
 initialState gameOverState = Game {
-    getSnake = [(startX, startY), (startX, startY-snakeLength)],
-    getFood = (3,3),
+    getSnake = [(startX, startY), (startX, startY-1), (startX, startY-2)],
+    getFood = (10,3),
     gameOver = gameOverState,
-    direction = DOWN
+    direction = DOWN,
+    randSeed = mkStdGen 100,
+    score = 0
 } where
-    startX = fromIntegral (width `div` 2)
-    startY = fromIntegral (height `div` 2)
+    startX = 15
+    startY = 15
 
+move :: SnakeGame -> Snake
+move snakeGame = 
+    if isFoodEaten then newHead : snake else newHead : init snake
+    where
+    (isFoodEaten, _) = (foodIsEaten snakeGame)
+    curDir = direction snakeGame
+    snake = (getSnake snakeGame)
+    newHead = 
+        -- (0, -1) is up
+        if curDir == UP then (xHead, yHead - 1)
+        -- (0, 1) is down
+        else if curDir == DOWN then (xHead, yHead + 1)
+        -- (-1, 0) is left
+        else if curDir == LEFT then (xHead - 1, yHead)
+        -- (0, 1) is right
+        else if curDir == RIGHT then (xHead + 1, yHead)
+        -- this last case shouldn't happen
+        else head snake
+
+    (xHead, yHead) = head snake
 
 changeDirection :: SnakeGame -> CurDir -> SnakeGame
-changeDirection (Game s f g direction) newDir = (Game s f g newDir)
+changeDirection (Game s f g direction r sc) newDir = (Game s f g newDir r sc)
+
+foodIsEaten :: SnakeGame -> (Bool, Int)
+foodIsEaten snakeGame = if (head (getSnake snakeGame)) == (getFood snakeGame)
+    then (True, score snakeGame + 1)
+    else (False, score snakeGame)
+
+checkGameOver :: SnakeGame -> Bool
+checkGameOver snakeGame = ((head $ getSnake snakeGame) `elem` (tail $ getSnake snakeGame)) ||
+    (fst (head $ getSnake snakeGame) <= 0|| fst (head $ getSnake snakeGame) >= gameBorderWidth) ||
+    (snd (head $ getSnake snakeGame) <= 0 || snd (head $ getSnake snakeGame) >= gameBorderHeight)
+
+generateFood :: SnakeGame -> (Food, StdGen)
+generateFood snakeGame = ((foodX, foodY), stdGen3)
+    where
+        (foodX, stdGen2) = ((randomR (1, gameBorderWidth-1) (randSeed snakeGame)))
+        (foodY, stdGen3) = ((randomR (1, gameBorderHeight-1) stdGen2))
